74df5dbd98ec647271f59b13a5e030369f1aa1de
davbianchi <davideb1912@gmail.com>
2019-11-15 10:43:50 +0100

    fixed bug in dialogue count

--- src/main/java/FreqMap.java
+++ src/main/java/FreqMap.java
@@ -45,7 +45,7 @@ public class FreqMap implements Map<String, HashMap<String, Float>> {
                     float upval = entry.getFrequencies().get(field) / entry.getFrequencies().get("nwords");
                     entry.getFrequencies().put(field, upval);
                 } else if (field.equals("dialogue")) {
-                    float dial = (entry.getFrequencies().get(field) / 2) / entry.getFrequencies().get("nwords");
+                    float dial = (entry.getFrequencies().get(field) / 2) / entry.getFrequencies().get("periods");
                     entry.getFrequencies().put(field, dial);
                 }
             }
