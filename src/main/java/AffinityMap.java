9a8224aa69daf88464af32e786545f997a6121c5
davbianchi <davideb1912@gmail.com>
2019-11-29 17:44:24 +0100

    testing new regex for closing dialogues

--- src/main/java/AffinityMap.java
+++ src/main/java/AffinityMap.java
@@ -18,7 +18,7 @@ public class AffinityMap implements Comparable {
      * Generates an instance with empty comparison map.
      *
      * @param auth the known author's name.
-     * @param unk the unknown author's name.
+     * @param unk  the unknown author's name.
      */
     public AffinityMap(String auth, String unk) {
         this.author = auth;
@@ -50,34 +50,56 @@ public class AffinityMap implements Comparable {
 
     @Override
     public int compareTo(Object o) {
-        AffinityMap rec = (AffinityMap) o;
-        HashMap<Integer, Integer> count = new HashMap<>();
-        count.put(-1, 0);
-        count.put(0, 0);
-        count.put(1, 0);
+//        AffinityMap rec = (AffinityMap) o;
+//        HashMap<Integer, Integer> count = new HashMap<>();
+//        count.put(-1, 0);
+//        count.put(0, 0);
+//        count.put(1, 0);
+//
+//        // count for compare values
+//        for (String s : this.map.keySet()) {
+//            if (this.map.get(s) < rec.map.get(s)) {
+//                count.put(-1, count.get(-1) + 1);
+//            } else if (rec.map.get(s) < this.map.get(s)) {
+//                count.put(1, count.get(1) + 1);
+//            } else {
+//                count.put(0, count.get(0) + 1);
+//            }
+//        }
+//
+//        // max on compare counts
+//        Map.Entry<Integer, Integer> max = null;
+//        int intmax = 0;
+//        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
+//            if (entry.getValue() > intmax) {
+//                intmax = entry.getValue();
+//                max = entry;
+//            }
+//        }
+//        assert max != null;
+//        return max.getKey();
 
-        // count for compare values
-        for (String s : this.map.keySet()) {
-            if (this.map.get(s) < rec.map.get(s)) {
-                count.put(-1, count.get(-1) + 1);
-            } else if (rec.map.get(s) < this.map.get(s)) {
-                count.put(1, count.get(1) + 1);
-            } else {
-                count.put(0, count.get(0) + 1);
+        // new comparison method
+
+        AffinityMap obj = (AffinityMap) o;
+        double thisAvg = 0.0;
+        double otherAvg = 0.0;
+        for (String field : this.map.keySet()) {
+            if (!field.equals("avg_period_length")){
+                thisAvg += this.map.get(field);
+                otherAvg += obj.map.get(field);
             }
         }
 
-        // max on compare counts
-        Map.Entry<Integer, Integer> max = null;
-        int intmax = 0;
-        for (Map.Entry<Integer, Integer> entry : count.entrySet()) {
-            if (entry.getValue() > intmax) {
-                intmax = entry.getValue();
-                max = entry;
-            }
-        }
-        assert max != null;
-        return max.getKey();
+        thisAvg /= 5;
+        otherAvg /= 5;
 
+        if (thisAvg < otherAvg) {
+            return -1;
+        } else if (thisAvg > otherAvg) {
+            return 1;
+        }
+
+        return 0;
     }
 }
